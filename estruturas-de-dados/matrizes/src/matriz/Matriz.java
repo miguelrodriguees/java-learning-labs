f64cd51fc1294032aeb78485c45b34334e8fe32d
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:08:32 -0300

    chore: adiciona estruturas-de-dados/matrizes/src/matriz/Matriz.java

---
+++ estruturas-de-dados/matrizes/src/matriz/Matriz.java
@@ -0,0 +1,87 @@
+
+package matriz;
+
+public class Matriz {
+
+    public static void main(String[] args) {
+        int [][] matriz = 
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        
+        };     
+        System.out.println("Elemento da linha 1, coluna 2: " + matriz[0][1]);
+        System.out.println("Elemento da linha 3, coluna 1: " + matriz[2][0]);
+        
+        System.out.println("-------------------------");
+        
+        for(int i = 0; i < matriz.length; i++){           
+            for(int j = 0; j < matriz[i].length; j++){
+                System.out.println(matriz[i][j] + " ");   
+            }
+        }
+    
+        int[][] matriz2 = new int[4][4];
+        matriz2[1][1] = 1; 
+        matriz2[1][2] = 2; 
+        matriz2[1][3] = 3; 
+        matriz2[2][1] = 4; 
+        matriz2[2][2] = 5; 
+        matriz2[2][3] = 6; 
+        matriz2[3][1] = 7; 
+        matriz2[3][2] = 8; 
+        matriz2[3][3] = 9; 
+              
+        System.out.println("Elemento da linha 1, coluna 2: " + matriz2[1][2]);
+        System.out.println("Elemento da linha 3, coluna 1: " + matriz2[3][1]);
+        
+        int soma = 0;
+        for(int i = 0; i < matriz2.length; i++){           
+            for(int j = 1; j < matriz2[i].length; j++){
+                soma += matriz2[i][j];
+                System.out.println("");
+                soma = 0;
+                
+            }
+        }
+       
+        int tirar = matriz [0][0];
+        for(int i = 0; i < 1; i++){           
+            for(int j = 0; j < matriz[i].length; j++){
+                tirar -= matriz[i][j];
+                System.out.println(tirar);
+            }        
+                System.out.println("subtração igual a: " + (i=1) + ("linha"));
+                tirar = 0;
+                
+            }
+        
+        
+        int maior = matriz[0][0];
+        for (int[] linha: matriz){
+            for (int valor: linha){
+                if (valor > maior){
+                    maior = valor;
+            }
+        }  
+      } 
+        System.out.println("Maior elemento: " + maior);
+        
+        
+        int menor = matriz[0][0];
+        for (int[] linha : matriz){
+            for (int valor: linha){
+                if (valor < menor){
+                    menor = valor;
+            }
+        }  
+      } 
+        System.out.println("Menor elemento: " + menor);
+        
+        }
+        
+    }
+
+   
+
