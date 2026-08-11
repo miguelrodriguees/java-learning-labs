27708c97e314ccd5fe3fa3e8fe083e87d0fa996e
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:12:48 -0300

    chore: adiciona poo/orientacao-objetos/src/orientacaoobjeto/SeguroDeVeiculo.java

---
+++ poo/orientacao-objetos/src/orientacaoobjeto/SeguroDeVeiculo.java
@@ -0,0 +1,27 @@
+
+package orientacaoobjeto;
+
+public class SeguroDeVeiculo extends Servicos{
+    private double valorSeguroVeiculo;
+    private double franquia;
+
+//get e set automaticos = ALT + INSERT
+    
+    public double getValorSeguroVeiculo() {
+        return valorSeguroVeiculo;
+    }
+
+    public void setValorSeguroVeiculo(double valorSeguroVeiculo) {
+        this.valorSeguroVeiculo = valorSeguroVeiculo;
+    }
+
+    public double getFranquia() {
+        return franquia;
+    }
+
+    public void setFranquia(double franquia) {
+        this.franquia = franquia;
+    }
+    
+    
+}
