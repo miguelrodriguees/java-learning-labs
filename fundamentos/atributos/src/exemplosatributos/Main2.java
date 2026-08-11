36e6ffabd3fb48ad848f54dd99fb0fcf7c19d395
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:07:41 -0300

    chore: adiciona fundamentos/atributos/src/exemplosatributos/Main2.java

---
+++ fundamentos/atributos/src/exemplosatributos/Main2.java
@@ -0,0 +1,13 @@
+
+package exemplosatributos;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        System.out.println(Configuracao.PI);
+        System.out.println(Configuracao.VERSAO);
+        
+        
+    }
+    
+}
