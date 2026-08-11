03866faf05394e76da6210a7e7d857706026d378
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:12:44 -0300

    chore: adiciona poo/orientacao-objetos/src/orientacaoobjeto/Gerente.java

---
+++ poo/orientacao-objetos/src/orientacaoobjeto/Gerente.java
@@ -0,0 +1,16 @@
+
+package orientacaoobjeto;
+ 
+ 
+public class Gerente {
+   String nome;
+   double salario;
+
+   void aumentaSalario(){
+      this.aumentaSalario(0.01);
+    }
+   
+   void aumentaSalario(double taxa){
+       this.salario += this.salario * taxa;
+    }
+}
