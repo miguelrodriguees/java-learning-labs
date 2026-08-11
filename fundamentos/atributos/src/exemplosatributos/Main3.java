352cccc881372e7484c5853c9de71b689a43ce98
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:07:46 -0300

    chore: adiciona fundamentos/atributos/src/exemplosatributos/Main3.java

---
+++ fundamentos/atributos/src/exemplosatributos/Main3.java
@@ -0,0 +1,16 @@
+
+package exemplosatributos;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+       Calculadora c = new Calculadora();
+       int resultadoSoma = c.somar(5 , 7);
+       
+        
+       System.out.println("Resultado zoado: " + c.resultado);
+       System.out.println("Resultado da soma:" + resultadoSoma);
+
+    
+   }
+ }
