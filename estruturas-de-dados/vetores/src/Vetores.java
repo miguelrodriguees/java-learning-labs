7e356cd38eb0a576c22083a5aa306c9d717bc5d9
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:09:18 -0300

    chore: adiciona estruturas-de-dados/vetores/src/Vetores.java

---
+++ estruturas-de-dados/vetores/src/Vetores.java
@@ -0,0 +1,21 @@
+
+public class Vetores {
+
+            
+    public static void main(String[] args) {
+       
+        int[] numeros = new int[5];
+        
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+    
+    
+        int[] outroValor = {10,20,30,40,50};
+        System.out.println("quarto elemento" + numeros[3]);
+        System.out.println("quinto elemento" + outroValor[4]);
+    }
+    
+}
