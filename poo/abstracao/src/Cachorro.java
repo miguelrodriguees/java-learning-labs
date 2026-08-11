e7e6a930780005a5df0fdf52b8b3f72aa7854465
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:10:47 -0300

    chore: adiciona poo/abstracao/src/Cachorro.java

---
+++ poo/abstracao/src/Cachorro.java
@@ -0,0 +1,16 @@
+
+public class Cachorro extends Animal {
+    public Cachorro (String nome){
+    super(nome);
+    }
+
+    
+    @Override
+    public void emitirSom(){
+       System.out.println("au au"); 
+    }
+  @Override
+  public void dormir(){
+       System.out.println("O cachorro esta dormindo"); 
+    }
+}
