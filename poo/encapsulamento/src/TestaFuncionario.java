9783ee74b1124fc4e3963a4b2e483392bb673b0c
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:11:08 -0300

    chore: adiciona poo/encapsulamento/src/TestaFuncionario.java

---
+++ poo/encapsulamento/src/TestaFuncionario.java
@@ -0,0 +1,22 @@
+
+public class TestaFuncionario {
+
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario();
+        f.setNome ("Funcionário Exemplo");
+        f.setSalario (2000);
+        f.setCodigoFunc (12345678);
+        
+        f.deposita(2500);
+        f.consultaDados();
+       
+      //System.out.println("nome do funcionario: " + f.getNome());
+      //System.out.println("salario do funcionario: " + f.getSalario());
+      //System.out.println("código do funcionário: " + f.getCodigoFunc());
+        
+        
+        
+        
+    }
+            
+}
