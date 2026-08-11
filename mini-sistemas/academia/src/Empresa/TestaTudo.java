34a3faaecfcfa9c9712b6f312691819ea70ca018
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:09:52 -0300

    chore: adiciona mini-sistemas/academia/src/Empresa/TestaTudo.java

---
+++ mini-sistemas/academia/src/Empresa/TestaTudo.java
@@ -0,0 +1,35 @@
+
+package Empresa;
+
+
+public class TestaTudo {
+
+    public static void main(String[] args) {
+       
+     Funcionario f = new Funcionario();
+       
+       f.setNome("Funcionário Exemplo");
+       f.setIdade(23);
+       f.setCargo("Professor de muay thai");     
+       f.setSalario(5000.);
+       f.setCadastro(270107);
+       f.setCpf("00000000000");
+       
+       
+     Clientes c = new Clientes();
+    
+       c.setNome("Cliente Exemplo");    
+       c.setIdade(24);
+       c.setCpf("11111111111");
+       c.setMatricula(12345678);
+       c.setPlano("trimestral");
+       c.setCadastro(193315);
+       c.setMensalidadePaga(true);
+       
+       
+       f.consultaDados();
+       c.verificarPagamento();
+       c.consultaDados();     
+    }
+    
+}
