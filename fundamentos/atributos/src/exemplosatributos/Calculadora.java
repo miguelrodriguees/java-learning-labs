661471c50c132cf8029c153693a62622b69a80d5
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:07:27 -0300

    chore: adiciona fundamentos/atributos/src/exemplosatributos/Calculadora.java

---
+++ fundamentos/atributos/src/exemplosatributos/Calculadora.java
@@ -0,0 +1,17 @@
+
+package exemplosatributos;
+
+public class Calculadora {
+    int resultado = 9876;
+    
+    int somar (int a, int b){
+    int resultado = a + b;
+    return resultado;
+    }
+    
+    
+    int subtracao(){
+    int resultadosubtracao = resultado -5;
+    return resultadosubtracao;
+    }
+}
