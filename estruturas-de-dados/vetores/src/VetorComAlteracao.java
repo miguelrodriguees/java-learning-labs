e39c323062dfaf63704d69341ba21ce6193096b9
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:09:14 -0300

    chore: adiciona estruturas-de-dados/vetores/src/VetorComAlteracao.java

---
+++ estruturas-de-dados/vetores/src/VetorComAlteracao.java
@@ -0,0 +1,15 @@
+
+public class VetorComAlteracao {
+    
+    public static void main(String[] args) {
+    int[] numeros = {10,20,30,40,50};
+ 
+    System.out.println("Vetor original: " );
+  
+    for (int num : numeros) {
+        System.out.println("Elemento: " + num);
+    }
+
+    
+}
+}
