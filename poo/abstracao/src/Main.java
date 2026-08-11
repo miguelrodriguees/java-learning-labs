f9c5c9235c3418f80dcea017bb606d63809499f0
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:11:00 -0300

    chore: adiciona poo/abstracao/src/Main.java

---
+++ poo/abstracao/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal cachorro = new Cachorro("Cachorro");
+        Animal gato = new Gato("Gato");
+        
+        
+       cachorro.emitirSom();
+       gato.emitirSom();
+       cachorro.dormir();
+    }
+    
+}
