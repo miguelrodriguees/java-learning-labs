19441b4b344a38acf68d86bd53146e90d4b4581a
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:13:09 -0300

    chore: adiciona poo/orientacao-objetos/src/orientacaoobjeto/testaGerente.java

---
+++ poo/orientacao-objetos/src/orientacaoobjeto/testaGerente.java
@@ -0,0 +1,22 @@
+
+package orientacaoobjeto;
+
+public class testaGerente {
+
+   
+    public static void main(String[] args) {
+       Gerente g = new Gerente();
+       
+       g.nome = "Gerente Exemplo";
+       g.salario = 1000;
+
+      g.aumentaSalario(0.01);
+        System.out.println("salario com 10% de aumento: " + g.salario);
+    
+      //g.aumentaSalario(0.015);
+        //System.out.println("novo aumento de 15%: " + g.salario);
+        
+        
+    }
+    
+}
