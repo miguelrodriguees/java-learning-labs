dbc7773318303a29668d086adc0a0a998bccb627
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:10:03 -0300

    chore: adiciona mini-sistemas/escola/src/Aluno.java

---
+++ mini-sistemas/escola/src/Aluno.java
@@ -0,0 +1,58 @@
+
+public class Aluno extends UsuarioEscola{
+   private double mediaNotas;
+   private String turma;
+   private String curso;
+   private String frequencia;
+
+    public double getMediaNotas() {
+        return mediaNotas;
+    }
+
+    public void setMediaNotas(double notas) {
+        this.mediaNotas = notas;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getFrequencia() {
+        return frequencia;
+    }
+
+    public void setFrequencia(String frequencia) {
+        this.frequencia = frequencia;
+    }
+   
+    public void consultaDados(){
+      super.consultaDados();
+        
+        System.out.println("Média de notas do aluno: " + getMediaNotas());
+        System.out.println("Turma do aluno: " + getTurma());
+        System.out.println("Curso do aluno: " + getCurso());
+        System.out.println("Frequecia do aluno: " + getFrequencia());
+        
+    }
+    public void calcularMedia(){
+        if (mediaNotas >= 6.0) {
+            System.out.println("Você passou de ano");
+        } else {
+            System.out.println("Você precisa melhorar a média");
+        }
+    }
+   
+    }
+
