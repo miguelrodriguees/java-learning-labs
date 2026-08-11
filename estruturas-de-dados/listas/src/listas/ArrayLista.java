bc29e14c74b0c57620995f14071adcf611bbcff1
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:08:12 -0300

    chore: adiciona estruturas-de-dados/listas/src/listas/ArrayLista.java

---
+++ estruturas-de-dados/listas/src/listas/ArrayLista.java
@@ -0,0 +1,21 @@
+
+package listas;
+
+import java.util.ArrayList;
+
+public class ArrayLista {
+
+    public static void main(String[] args) {
+        ArrayList<String> frutas = new ArrayList<>();
+        
+        frutas.add("Goiaba");
+        frutas.add("Morango");
+        frutas.add(0, "Banana");
+        frutas.add("Maçã");
+        frutas.add("Laranja");
+       
+        System.out.println("ArrayList -> " + frutas);
+        System.out.println("Posição -> " + frutas.get(2));
+    }
+    
+}
