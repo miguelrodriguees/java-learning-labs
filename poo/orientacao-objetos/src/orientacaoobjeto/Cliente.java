f655065b9c06e99926a28c3a3dea3c97ebc8e16d
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:12:35 -0300

    chore: adiciona poo/orientacao-objetos/src/orientacaoobjeto/Cliente.java

---
+++ poo/orientacao-objetos/src/orientacaoobjeto/Cliente.java
@@ -0,0 +1,16 @@
+package orientacaoobjeto;
+
+
+public class Cliente {
+    String nome;
+    double salario;
+          
+    
+    void aumentaSalario(){
+    }
+    
+    void aumentaSalario(double taxa){
+    this.salario += this.salario * taxa;
+        
+    }
+}
