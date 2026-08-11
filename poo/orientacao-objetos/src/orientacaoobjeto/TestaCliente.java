0ab8bcd5e879298dd2f83c7c6fea21d9a9f9d741
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:13:00 -0300

    chore: adiciona poo/orientacao-objetos/src/orientacaoobjeto/TestaCliente.java

---
+++ poo/orientacao-objetos/src/orientacaoobjeto/TestaCliente.java
@@ -0,0 +1,27 @@
+
+package orientacaoobjeto;
+
+public class TestaCliente {}
+
+    //public static void main(String[] args) {
+      //Cliente c1 = new Cliente();
+      
+     // c1.codigo = 12;
+      //c1.nome = "Cliente 1";
+      ///Cliente c2 = new Cliente();
+      
+      //c2.codigo = 13;
+      //c2.nome = "Cliente 2";
+        
+      //System.out.println( "total: " + c1.codigo);   
+      //System.out.println("nome: " + c1.nome);
+  
+      
+      //System.out.println("total: " + c2.codigo);
+      //System.out.println("nome: " + c2.nome);
+  
+    
+    
+
+
+
