f6d76825b08751b89755802dc6bca4556eb3de2e
Miguel Rodrigues Fernandes de Lima <miguelrodrigues5870@gmail.com>
2026-08-11 10:10:55 -0300

    chore: adiciona poo/abstracao/src/Gato.java

---
+++ poo/abstracao/src/Gato.java
@@ -0,0 +1,18 @@
+
+public class Gato extends Animal {
+    public Gato(String nome){
+    super(nome);
+    }
+
+    
+    @Override
+    public void emitirSom(){
+    System.out.println("miau"); 
+  
+    }
+
+    @Override
+    public void dormir() {
+        System.out.println("O gato está dormindo");
+    }
+}
